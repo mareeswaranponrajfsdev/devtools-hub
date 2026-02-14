5846d9baf990126a9591b0ee17ccdd40bff94e18
Karthik <karthikmathavan.fsdev@gmail.com>
2026-02-14 12:11:46 +0530

    removed the src folder.

--- src/src/app/features/json-formatter/utils/json-error.util.spec.ts
+++
@@ -1,7 +0,0 @@
-import { JsonError } from './json-errorutil';
-
-describe('JsonError', () => {
-  it('should create an instance', () => {
-    expect(new JsonError()).toBeTruthy();
-  });
-});
--- src/src/environments/environment.prod.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-  production: true,
-
-  emailJs: {
-    publicKey: 'K7ibQbYrYla_EPW-V',
-    serviceId: 'service_9x7v1mt',
-    templateId: 'template_q13r9dt'
-  }
-};
